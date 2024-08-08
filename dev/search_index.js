e14c4708c50274eeedefbd1bc0fb6db8b68df9b6
Documenter.jl <documenter@juliadocs.github.io>
2024-08-08 15:46:15 +0000

    build based on 0298811

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HypoExponential","category":"page"},{"location":"#HypoExponential","page":"Home","title":"HypoExponential","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HypoExponential.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HypoExponential]","category":"page"}]
+}
